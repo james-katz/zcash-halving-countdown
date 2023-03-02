b5b3de0fe2f9eb6d36949451c790a4e9ad26045f
james-katz <james.j.katz@protonmail.com>
2023-03-02 12:55:03 -0300

    Express + better code

--- index.js
+++ index.js
@@ -3,6 +3,15 @@ const PROTO_PATH = './service.proto';
 const grpc = require('@grpc/grpc-js');
 const protoLoader = require('@grpc/proto-loader');
 
+const express = require('express');
+const app = express();
+const port = 3001;
+
+const halvingInterval = 1680000;
+const firstHalving = 1046400;
+const avgBlockTime = 75;
+const serverUri = 'mainnet.lightwalletd.com:9067';
+
 const packageDefinition = protoLoader.loadSync(
     PROTO_PATH,
     {keepCase: true,
@@ -11,46 +20,64 @@ const packageDefinition = protoLoader.loadSync(
      defaults: true,
      oneofs: true
 	});
+
 const compactTxStreamer = grpc.loadPackageDefinition(packageDefinition).cash.z.wallet.sdk.rpc.CompactTxStreamer;
-const client = new compactTxStreamer('mainnet.lightwalletd.com:9067', grpc.credentials.createSsl());
+const client = new compactTxStreamer(serverUri, grpc.credentials.createSsl());
 
 const chainSpec = new grpc.loadPackageDefinition(packageDefinition).cash.z.wallet.sdk.rpc.ChainSpec;
 
-client.GetLatestBlock(chainSpec, (err, res) => {
-	if(err) throw err;
-	const height = res.height;
-    const nextHalving = nextHalvingBlock(height);
-    console.log(`Next halving height: ${nextHalving}`);
-    console.log(`Blocks until next halving: ${nextHalving - height}`);
+function countdown() {   
+    return new Promise((resolve, reject) => {
+        client.GetLatestBlock(chainSpec, (err, res) => {
+            if(err) reject(err);
+
+            const height = res.height;            
+            const nextHalving = nextHalvingBlock(height);
+            const remainingBlocks = nextHalving - height;
+            const secondsToHalving = avgBlockTime * remainingBlocks;
+            
+            const countdownDay = Math.floor(secondsToHalving / (3600*24));
+            const countdownHrs = Math.floor(secondsToHalving % (3600*24) / 3600);
+            const countdownMin = Math.floor(secondsToHalving % 3600 / 60);
+            const countdownSec = Math.floor(secondsToHalving % 60);
+
+            const halvingDate = new Date();
+            halvingDate.setSeconds(secondsToHalving);                        
+
+            const json = {
+                "height": height,
+                "next_halving": nextHalving,
+                "remaining_blocks": remainingBlocks,                
+                "countdown": {
+                    "secs": countdownSec,
+                    "mins": countdownMin,
+                    "hours": countdownHrs,
+                    "days": countdownDay,
+                }
+                ,
+                "halving_date": halvingDate
+            }
+
+            resolve(json);
+        });
+    });    
+}
+
+function nextHalvingBlock(height) {    
+    let next = firstHalving;
+    
+    while(next <= height) {
+        next += halvingInterval;
+    }
+    return next;
+}
+
+app.get('/', async (req, res) => {
+    countdown()
+    .then((json) => res.json(json))
+    .catch((err) => res.send(err));
 });
 
-function nextHalvingBlock(height) {
-    let halving = calculateHalvingNumber(height);
-    let nextHalving = halving + 1;
-    let halvingHeight = height;
-    
-    while(halving < nextHalving) {
-        halvingHeight ++;
-        halving = calculateHalvingNumber(halvingHeight);        
-    }
-
-    return halvingHeight;
-}
-
-function calculateHalvingNumber(height) {
-    const blossomActivationHeight = 653600;
-    const preBlossomHalvingInterval = 840000;
-    
-    const preBlossomPoWTargetSpacing = 150;
-    const postBlossomPoWTargetSpacing = 75;
-
-    const blossomPoWTargetSpacingRatio = preBlossomPoWTargetSpacing / postBlossomPoWTargetSpacing;    
-    const postBlossomHalvingInterval = Math.floor(preBlossomHalvingInterval * blossomPoWTargetSpacingRatio)
-
-    const slowStartInterval = 20000;
-    const slowStartShift = slowStartInterval / 2;
-
-    const halving = Math.floor(( (blossomActivationHeight - slowStartShift) / preBlossomHalvingInterval ) + ( (height - blossomActivationHeight) / postBlossomHalvingInterval ));
-
-    return halving;
-}
+app.listen(port, () => {
+    console.log(`Express listening on port: ${port}`);
+});
